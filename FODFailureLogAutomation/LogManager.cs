365697e63b73b40279c47e59597bd833cc1e972c
A. Patricio <thqv74@gmail.com>
2023-06-23 15:17:02 -0300

    Refactor - Class LogManager created

---
+++ FODFailureLogAutomation/LogManager.cs
@@ -0,0 +1,81 @@
+﻿
+using System.IO;
+using System.Windows.Forms;
+
+namespace FODFailureLogAutomation
+{
+    class LogManager
+    {
+        string strLogFileName = "inline_log.txt";
+        string strTrackIdErrorMsg = "TrackId Not Found!!!";
+        string strPicturePattern = "*.bmp*";
+        FrmMain frmMn = FrmMain.getInstance();
+        public bool getFailureLog()
+        {
+            try
+            {
+                foreach (string file_name in Directory.GetFiles(frmMn.textBoxDirectory.Text + "\\" + frmMn.textBoxTrackId.Text + "\\", strLogFileName, SearchOption.AllDirectories))
+                {
+                    using (var reader = new StreamReader(file_name))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            linkLogWithBoxes(line);
+                        }
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show(strTrackIdErrorMsg);
+                frmMn.textBoxTrackId.Text = "";
+                return false;
+            }
+        }
+        public void linkLogWithBoxes(string line)
+        {
+            if (line.Contains("[MMI_RESULT]") && line.Contains("failed"))
+            {
+                frmMn.listBoxMeasCode.Items.Add(line);
+            }
+            if (line.Contains("TH:["))
+            {
+                line = line.Replace("[MMI_TH]", "");
+                frmMn.listBoxSpecsLimit.Items.Add(line);
+            }
+            if (line.Contains("[MMI_Calibraiton]"))
+            {
+                line = line.Replace("[MMI_Calibraiton]", "");
+                if (!line.Contains("Calibration Test Result:fail"))
+                    frmMn.listBoxResultFailure.Items.Add(line);
+            }
+        }
+        public void clearAllLog()
+        {
+            frmMn.listBoxMeasCode.Items.Clear();
+            frmMn.listBoxSpecsLimit.Items.Clear();
+            frmMn.listBoxResultFailure.Items.Clear();
+            frmMn.comboBoxFailurePictures.Items.Clear();
+        }
+        public void getPictureLog()
+        {
+            try
+            {
+                string pictureModelCheck = "Godix";
+                foreach (string pictureName in Directory.GetFiles(frmMn.textBoxDirectory.Text + "\\" + frmMn.textBoxTrackId.Text + "\\", strPicturePattern, SearchOption.AllDirectories))
+                {
+                    frmMn.comboBoxFailurePictures.Items.Add(pictureName);
+                    pictureModelCheck = "Egis";
+                }
+                frmMn.labelProvider.Text = pictureModelCheck;
+            }
+            catch
+            {
+                MessageBox.Show(strTrackIdErrorMsg);
+            }
+        }
+
+    }
+}
